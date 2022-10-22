c22d56092f6254e980a30aa129ed16f280d35ed0
治怒善男 <1377875184@qq.com>
2022-10-22 21:45:51 +0800

    商户缓存

--- hm-dianping/src/main/java/com/hmdp/service/IShopService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IShopService.java
@@ -1,11 +1,12 @@
 package com.hmdp.service;
 
-import com.hmdp.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 虎哥
@@ -13,4 +14,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+    /**
+     * 根据id查询商铺
+     *
+     * @param id 商铺id
+     * @return 返回统一返回值
+     */
+    Result queryById(Long id);
 }
